116bd0f07a792e76adf2742d3b165c55ced24b02
JFlores100 <juanfloresp71@gmail.com>
2023-12-13 11:05:46 -0400

    mi examen final

---
+++ src/modelo/Conexion.py
@@ -0,0 +1,13 @@
+import psycopg2
+
+from decouple import config
+
+def conexion2023():
+    conex=psycopg2.connect(
+                        host=config('BD_HOST'),
+                        user=config('BD_USER'),
+                        password=config('BD_PASSWORD'),
+                        database=config('BD_DB'),
+                        port=5432
+                        )
+    return conex
